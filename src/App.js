13f61bd54eec31409d74e2a1f70c504c27f8d5a9
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-04-16 21:01:53 -0700

    styled dropdown and textbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import TextBox from "./components/TextBox";
+import Arrows from "./components/Arrows";
+import Button from "./components/button";
+import Modal from "./components/Modal";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <TextBox style = {"input"}/>
+     <TextBox style = {"output"}/>
     </div>
   );
 }
---
+++ src/components/TextBox.js
@@ -0,0 +1,17 @@
+import SelectDropDown from "./SelectDropDown";
+
+const TextBox = ({style}) => {
+    return(
+        <div className = {style}>
+            <SelectDropDown/>
+            <textarea
+             placeholder ={style == "input" ? "Enter Text": "Translation"}
+             disabled = {style == "output"}
+            
+            
+            />
+        </div>
+    )
+}
+
+export default TextBox;
